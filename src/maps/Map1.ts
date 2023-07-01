07c0c9f8592ef8946d3226478d42d626564a8b9c
furuTra <tktk3008take@gmail.com>
2023-07-01 04:57:43 +0000

    マップに衝突判定を追加

--- src/maps/Map1.ts
+++ src/maps/Map1.ts
@@ -1,31 +1,37 @@
 import Phaser from 'phaser';
+import { TLayer, TTileMap } from './types';
 import mapTile from '@assets/maps/map1/map.png';
 import mapJson from '@assets/maps/map1/map.json';
 
 export default class Map1 {
   private scene: Phaser.Scene;
 
-  private map?: Phaser.Tilemaps.Tilemap;
+  private map: Phaser.Tilemaps.Tilemap;
 
-  private tileset?: Phaser.Tilemaps.Tileset;
+  private tileset: Phaser.Tilemaps.Tileset;
 
   constructor(scene: Phaser.Scene) {
     this.scene = scene;
-  }
-
-  static preload(scene: Phaser.Scene) {
-    scene.load.image('tiles', mapTile);
-    scene.load.tilemapTiledJSON('map', mapJson);
-  }
-
-  create() {
     this.map = this.scene.make.tilemap({
       key: 'map',
     });
     this.tileset = this.map.addTilesetImage('map', 'tiles', 96, 96, 0, 0);
-    this.map.createLayer('base', this.tileset);
-    this.map.createLayer('wall', this.tileset);
-    this.map.createLayer('floor1', this.tileset);
-    this.map.createLayer('floor2', this.tileset);
+  }
+
+  static preload(scene: Phaser.Scene) {
+    scene.load.image('tiles', mapTile);
+    console.log(mapJson);
+    scene.load.tilemapTiledJSON('map', mapJson);
+  }
+
+  create() {
+    (mapJson as TTileMap).layers.forEach((layer: TLayer) => {
+      if (layer.type === 'tilelayer') {
+        const mapLayer = this.map.createLayer(layer.name, this.tileset).setCollisionByProperty({
+          collides: true,
+        });
+        this.scene.matter.world.convertTilemapLayer(mapLayer);
+      }
+    });
   }
 }
