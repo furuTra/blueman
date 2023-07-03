7d894c21d09e7814b8935658a2a1465fa07e7ad3
furuTra <tktk3008take@gmail.com>
2023-07-03 16:10:57 +0000

    HPの割合が表示されるよう修正

--- src/scenes/UIScene.ts
+++ src/scenes/UIScene.ts
@@ -9,33 +9,42 @@ export default class UIScene extends Phaser.Scene {
 
   private _property?: Property;
 
+  private _playerName?: Phaser.GameObjects.Text;
+
+  private _playerHPBar?: Phaser.GameObjects.Graphics;
+
+  private _playerHP?: Phaser.GameObjects.Text;
+
   constructor() {
     super({ key: 'ui_scene' });
   }
 
   init() {
     this._property = this.registry.get('player');
+    this._playerHPBar = this.add.graphics();
+    this.createPlayerHeader();
+    this.createPlayerHP();
   }
 
   draw() {
-    if (!this._property) return;
+    if (!this._property || !this._playerHPBar) return;
+    this._playerHPBar.clear();
     const healthBarLength = 480;
     const healthBarHeight = 20;
     const perHealth = this._property.hp / this._property.maxHp;
-    const graphics = this.add.graphics();
-    graphics
+    this._playerHPBar
       .fillStyle(0x4d4d4d)
       .fillRect(this._hpBarPos.x, this._hpBarPos.y, healthBarLength, healthBarHeight);
-    graphics.fillStyle(0x80ff80);
+    this._playerHPBar.fillStyle(0x80ff80);
     if (perHealth <= 0.5) {
       // Health(Warn)
-      graphics.fillStyle(0xffc14d);
+      this._playerHPBar.fillStyle(0xffc14d);
     }
     if (perHealth <= 0.2) {
       // Health(Danger)
-      graphics.fillStyle(0xff6666);
+      this._playerHPBar.fillStyle(0xff6666);
     }
-    graphics.fillRect(
+    this._playerHPBar.fillRect(
       this._hpBarPos.x + 1,
       this._hpBarPos.y + 1,
       perHealth * (healthBarLength - 2),
@@ -57,12 +66,31 @@ export default class UIScene extends Phaser.Scene {
 
   createPlayerHeader() {
     if (!this._property) return;
-    this.add.text(this._namePos.x, this._namePos.y, this._property?.name, {
+    this._playerName = this.add.text(this._namePos.x, this._namePos.y, this._property?.name, {
       fontFamily: 'arial',
       fontSize: '18px',
       color: '#f0f8ff',
       backgroundColor: '#4d4d4d',
     });
+  }
+
+  createPlayerHP() {
+    if (!this._playerName || !this._property) return;
+    this._playerHP = this.add.text(
+      this._namePos.x + this._playerName.width + 10,
+      this._namePos.y,
+      `${this._property.hp} / ${this._property.maxHp}`,
+      {
+        fontFamily: 'arial',
+        fontSize: '18px',
+        color: '#f0f8ff',
+        backgroundColor: '#4d4d4d',
+      }
+    );
+  }
+
+  update() {
+    this._playerHP?.setText(`${this._property?.hp} / ${this._property?.maxHp}`);
   }
 
   decrease(amount: number) {
