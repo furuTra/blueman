8619abc90ac868dc1266994e5a5093a086aaa1b7
furuTra <tktk3008take@gmail.com>
2023-07-05 17:30:42 +0000

    不要なタイプを修正

--- src/components/characters/BodyLists.ts
+++ src/components/characters/BodyLists.ts
@@ -2,11 +2,10 @@ import Phaser from 'phaser';
 import { ICharacter } from './interfaces';
 import Droid from './Droid';
 import Blue from './Blue';
-
-export type TBodyType = 'droid' | 'blue';
+import { TBodyKey } from './types';
 
 const getBodyLists = (scene: Phaser.Scene, x: number, y: number) => {
-  const bodyLists = new Map<TBodyType, () => ICharacter>();
+  const bodyLists = new Map<TBodyKey, () => ICharacter>();
   bodyLists.set('droid', () => new Droid(scene, x, y));
   bodyLists.set('blue', () => new Blue(scene, x, y));
   return bodyLists;
