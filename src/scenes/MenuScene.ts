8df980b6c5504b85a5631e003b56554871f0b218
furuTra <tktk3008take@gmail.com>
2023-07-16 07:36:53 +0000

    キャンセルボタンと決定ボタンを作成してみる。

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -1,9 +1,13 @@
 import Phaser from 'phaser';
 import CrossIcon from '@assets/icons/cross.png';
+import ReturnIcon from '@assets/icons/return.png';
+import CheckmarkIcon from '@assets/icons/checkmark.png';
 import eventsCenter from '~/events/EventsCenter';
 
 export default class MenuScene extends Phaser.Scene {
   static readonly sceneKey = 'menu_scene';
+
+  private _backBoard?: Phaser.GameObjects.Graphics;
 
   constructor() {
     super(MenuScene.sceneKey);
@@ -11,13 +15,15 @@ export default class MenuScene extends Phaser.Scene {
 
   preload() {
     this.load.image('cross', CrossIcon);
+    this.load.image('return', ReturnIcon);
+    this.load.image('checkmark', CheckmarkIcon);
   }
 
   create() {
-    const graphics = this.add.graphics();
-    graphics.fillStyle(0xffcc33, 1);
-    graphics.fillRect(100, 200, 600, 300);
+    this._backBoard = this.add.graphics();
+    this._backBoard.fillStyle(0xffcc33, 1).fillRect(100, 200, 600, 300);
 
+    // ×ボタン
     this.add
       .image(650, 200, 'cross')
       .setOrigin(0)
@@ -31,5 +37,40 @@ export default class MenuScene extends Phaser.Scene {
         },
         this
       );
+
+    // キャンセルボタン
+    this.createButton(100, 500, 'cancel', 'return', () => {
+      console.log('cancel');
+    });
+
+    // 決定ボタン
+    this.createButton(200, 500, 'ok!', 'checkmark', () => {
+      console.log('ok');
+    });
+  }
+
+  createButton(x: number, y: number, label: string, icon: string, callback: () => void) {
+    const iconImg = this.add.image(0, 0, icon);
+    const button = this.add
+      .text(0, 0, label, {
+        fontFamily: 'arial',
+        fontSize: '32px',
+        color: '#ffffff',
+        align: 'center',
+        backgroundColor: '#f2bd61',
+      })
+      .setOrigin(0, 0.5)
+      .setPadding({ left: iconImg.width })
+      .setInteractive()
+      .on('pointerdown', callback)
+      .on('pointerover', () => {
+        button.setStyle({ fill: '#f39c12' });
+        iconImg.setTintFill(0xf39c12);
+      })
+      .on('pointerout', () => {
+        button.setStyle({ fill: '#ffffff' });
+        iconImg.setTintFill(0xffffff);
+      });
+    this.add.container(x, y, [button, iconImg]);
   }
 }
